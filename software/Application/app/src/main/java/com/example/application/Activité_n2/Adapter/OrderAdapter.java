14a7b3cdd7c28f5e70f18dada0cc3432fd55893a
Louis Malbranque <louis.malbranque@gmail.com>
2019-04-12 12:13:43 +0200

    ajout layout liste d'ordre

--- software/Application/app/src/main/java/com/example/application/Activité_n2/Adapter/OrderAdapter.java
+++ software/Application/app/src/main/java/com/example/application/Activité_n2/Adapter/OrderAdapter.java
@@ -35,6 +35,15 @@ public class OrderAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
         }
     }
 
+    class DefaultViewHolder extends RecyclerView.ViewHolder {
+
+        public DefaultViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+
+
     public OrderAdapter(Context c, List<Order> orderList) {
         context=c;
         this.orderList = orderList;
@@ -45,6 +54,7 @@ public class OrderAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
 
     @Override
     public int getItemViewType(int position) {
+        System.out.println(ListOrder.get(position).getType());
         if (ListOrder.get(position).getType().equals("ProgrammedMode")){
             return 0;
         }
@@ -63,10 +73,11 @@ public class OrderAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        System.out.println("on create view holder");
         switch(i){
-            case 0: return new ProgrammedViewHolder(LayoutInflater.from(context).inflate(R.layout.fragment_programme,viewGroup,false));
-            case 1: return new RealTimeViewHolder(LayoutInflater.from(context).inflate(R.layout.fragment_temps_reel,viewGroup,false));
-            default: return new ProgrammedViewHolder(LayoutInflater.from(context).inflate(R.layout.fragment_programme,viewGroup,false));
+            case 0: return new ProgrammedViewHolder(LayoutInflater.from(context).inflate(R.layout.ordre_programme_liste,viewGroup,false));
+            case 1: return new RealTimeViewHolder(LayoutInflater.from(context).inflate(R.layout.ordre_temps_reel_liste,viewGroup,false));
+            default: return new DefaultViewHolder(LayoutInflater.from(context).inflate(R.layout.ordre_defaut_liste,viewGroup,false));
         }
 
     }
