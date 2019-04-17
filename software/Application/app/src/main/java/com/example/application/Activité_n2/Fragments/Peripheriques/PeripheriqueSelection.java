26bfec9f275b1ed1429c0b82c307689c8d6bba2b
Val <val62.fav@gmail.com>
2019-04-17 17:41:15 +0200

    changement layout + correctif button

--- software/Application/app/src/main/java/com/example/application/Activité_n2/Fragments/Peripheriques/PeripheriqueSelection.java
+++ software/Application/app/src/main/java/com/example/application/Activité_n2/Fragments/Peripheriques/PeripheriqueSelection.java
@@ -13,7 +13,6 @@ import android.widget.Button;
 
 import com.example.application.Activité_n2.Adapter.PeripheriqueSelectionAdapter;
 import com.example.application.Activité_n2.Fragments.Menu.Menu;
-import com.example.application.Activité_n2.Fragments.Programmé.Programme;
 import com.example.application.R;
 
 import java.util.ArrayList;
@@ -46,8 +45,11 @@ public class PeripheriqueSelection extends Fragment {
         peripheriquesRecycler.setLayoutManager(layoutManager);
         peripheriquesRecycler.setItemAnimator( new DefaultItemAnimator());
         peripheriquesRecycler.setAdapter(peripheriqueAdapter);
-
         envoyer = v.findViewById(R.id.envoyer_peripherique_selection);
+
+
+
+
         envoyer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,12 +58,19 @@ public class PeripheriqueSelection extends Fragment {
                     if (p.isConnecte()) data+=",1";
                     else data+=",0";
                 }
+                envoyer.setEnabled(false);
                 //com.example.application.Activité_n1.Bluetooth.Peripherique.peripherique.envoyer(data);
                 System.out.println(data);
-                getFragmentManager().beginTransaction().replace(R.id.fragment, Menu.menu).addToBackStack(null).commit();
+
+
+                getFragmentManager().beginTransaction().replace(R.id.fragment, Menu.menu).commit();
+
             }
         });
 
+
+
         return v;
     }
+
 }
